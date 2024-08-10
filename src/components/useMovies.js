16b84c603bdfdded93d7d1f6c02396a29769b17e
Saavan-Dev <crosslpdgajjar@gmail.com>
2024-08-11 00:42:41 +0530

    corrected url related to the API in useMovies

--- src/components/useMovies.js
+++ src/components/useMovies.js
@@ -13,7 +13,7 @@ export function useMovies(query) {
           setLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
